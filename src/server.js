f8afed21375440455f9da8ae6cf2453e7ce85c5b
Kenny Hatch <kah8090@rit.edu>
2022-02-13 16:12:59 -0500

    completed http-api-assignment-ii

---
+++ src/server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler =  require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    'GET': {
+        '/': htmlHandler.getIndex,
+        '/style.css':htmlHandler.getCSS,
+        '/getUsers': jsonHandler.getUsers,
+        '/addUser':jsonHandler.addUser,
+        '/notReal': jsonHandler.notReal
+    },
+    'HEAD':{
+        '/getUsers': jsonHandler.getUsersMeta,
+        '/notReal': jsonHandler.notRealMeta,
+    },
+};
+
+const handlePost = (request, response, parsedUrl) => {
+    if(parsedUrl.pathname === '/addUser') {
+      parseBody(request, response, jsonHandler.addUser);
+    }
+};
+
+const parseBody = (request, response, handler) => {
+    const body = [];
+  
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+  
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+  
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+  
+      handler(request, response, bodyParams);
+    });
+};
+
+const onRequest = (request, response) => {
+    const parsedUrl = url.parse(request.url);
+    
+    if (request.method === 'POST') {
+        handlePost(request, response, parsedUrl);
+    } else {
+        if(urlStruct[request.method][parsedUrl.pathname]){
+            urlStruct[request.method][parsedUrl.pathname](request, response);
+          } else {
+            urlStruct[request.method].notReal(request, response);
+        }
+    }
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1: ${port}`);
+});
